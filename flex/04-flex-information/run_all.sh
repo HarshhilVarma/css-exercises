bccc500344e06a27b02d6decb9462cc9fdcc459f
HarshhilVarma <109222912+HarshhilVarma@users.noreply.github.com>
2024-08-21 20:26:09 +0530

    Finished multiple exercises

---
+++ flex/04-flex-information/run_all.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+echo "Today is" `date`whc
+echo -e "\nenter the path to the directory"
+read the_path
+
+echo -e "\n your path has the following files and folders: "
+ls $the_path
